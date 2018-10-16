4f4d022ce0f2e092c3a56bcccaf691a0d2352327
ath <ath@pfb05.cshl.edu>
2018-10-16 15:45:22 -0400

    Fixed repository

---
+++ Python_Problemsets/first_script.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+#This is the script for Python 1 problem set
+import sys
+MyName = sys.argv[1]
+FavoriteColor = sys.argv[2]
+FavoriteActivity = sys.argv[3]
+FavoriteAnimal = sys.argv[4]
+
+print ('My name is', MyName, '\n', 'My favorite color is', FavoriteColor, '\n', 'My favorite activity is',
+FavoriteActivity, '\n', 'My favorite animal is a', FavoriteAnimal)
---
+++ Python_Problemsets/problemset2.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+mystery = 28
+
+if mystery > 100:
+	print ('True')
+else:
+	print ('Not true') 
