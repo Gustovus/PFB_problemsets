87b8ebaa39bf679e2f9cfaf53e8906d55c3b5cea
ath <ath@pfb05.cshl.edu>
2018-10-17 12:24:40 -0400

    Updated the code

--- Python_Problemsets/problemset3_eco.py
+++ Python_Problemsets/problemset3_eco.py
@@ -8,6 +8,9 @@ dna = dna.upper()
 ecor1start = 'GAATTC'
 dna_start = dna.find(ecor1start)
 
-print(((dna_start)+1), "is the start site and", ((dna_start)+6), "is the end site")
+dnastring = "{} is the start site and {} is the end site"
+print(dnastring.format(dna_start+1, dna_start+6))
 
+print(((dna_start)+1), "is the start site and", ((dna_start)+6), "is the end site")
+print(dna[(dna_start):(dna_start+6)])
 
