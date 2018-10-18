0a9e6ac57c94ad9119d32510d23fcae3406a1ccc
ath <ath@pfb05.cshl.edu>
2018-10-17 20:20:50 -0400

    Updated problemset 4

--- Python_Problemsets/problemset4_makelist.py
+++ Python_Problemsets/problemset4_makelist.py
@@ -8,3 +8,7 @@ for seq in dna:
 	print(str(count)+'\t'+str(len(seq))+'\t'+seq+'\n')
 	count += 1
 
+tupleseq = [(seq, len(seq)) for seq in dna]
+
+for tup in tupleseq:
+	print(tup[1],'\t',tup[0],'\n')
