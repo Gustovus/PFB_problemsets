8b1c8af48882a6d72cd3e9241ec780667a669787
ath <ath@pfb05.cshl.edu>
2018-10-18 20:26:17 -0400

    Finished the enzyme ya bish

--- Python_Problemsets/problemset7_6enzyme.py
+++ Python_Problemsets/problemset7_6enzyme.py
@@ -4,11 +4,22 @@ import re
 
 seq1 = open('Python_07_ApoI.fasta', 'r')
 
-seq1read = seq1.read()
+seq1readhead = seq1.read()
+
+seq1read = re.sub(r'(>seq1\n)', r'', seq1readhead)
+
+seq1read = seq1read.replace('\n', '')
 
 for found in re.finditer(r"[GA]AATT[CT]", seq1read):
 	print(found.start(0)+1)
 
 cutseq = re.sub(r'([GA])(AATT[CT])', r'\1^\2', seq1read)
-print(cutseq)
+
+
+cutfrags = cutseq.split('^')
+
+sortfrags = sorted(cutfrags, key=len)
+
+sortfrags = sortfrags[::-1]
+print(sortfrags)
 
